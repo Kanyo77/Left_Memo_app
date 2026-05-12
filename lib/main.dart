35d0047417b980d71647a6844025ee1d73a6e59c
Trần Khoa <126307951+Kanyo77@users.noreply.github.com>
2026-05-13 06:11:49 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
 
-      // kích thước thiết kế gốc
       designSize: const Size(390, 844),
 
       minTextAdapt: true,
